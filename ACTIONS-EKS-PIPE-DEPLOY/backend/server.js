c11c3d02dfc8a8faa8378ce21fdeadc719f62870
Nihanth-sai <bandaru.nihanthsai@hcltech.com>
2026-06-17 11:07:49 +0000

    checker

--- ACTIONS-EKS-PIPE-DEPLOY/backend/server.js
+++ ACTIONS-EKS-PIPE-DEPLOY/backend/server.js
@@ -24,7 +24,7 @@ app.get("/health", (req, res) => {
 
 app.get("/api/test", (req, res) => {
   res.json({
-    message: "Backend Connected Successfully and ready to roll"
+    message: "Backend Connected Successfully and ready to roll out"
   });
 });
 
